a8150e52205dcef73b6700c3675ce296e17e949c
barabat <mads.sorhaug@gmail.com>
2011-08-26 22:46:33 +0200

    Changed square color

--- src/com/barabatz/firstrts/texture/SquareEntity.java
+++ src/com/barabatz/firstrts/texture/SquareEntity.java
@@ -19,7 +19,7 @@ public class SquareEntity {
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
         //Set quad color (R,G,B)
-        GL11.glColor3f(0.5f, 0.5f, 1.0f);
+        GL11.glColor3f(0.5f, 0.8f, 1.0f);
 
         //Draw a quad
         GL11.glBegin(GL11.GL_QUADS);
